472f811fdb5918fc668d92ea1ff0091c17533142
IliaDushnoi <iliatana133@gmail.com>
2025-04-18 12:45:12 +0500

    Add files via upload

---
+++ School_Journal/School_Journal/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Journal
+{
+    public class Student
+    {
+        public string name;
+        public int age;
+        public double averageGrade;
+
+        public Student(string name, int age, double averageGrade)
+        {
+            this.name = name;
+            this.age = age;
+            this.averageGrade = averageGrade;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public double AverageGrade {  get => averageGrade; set => averageGrade = value; }
+
+        public override string ToString()
+        {
+            return $"{name} ({age} лет) - Оценка: {averageGrade}";
+        }
+    }
+}
